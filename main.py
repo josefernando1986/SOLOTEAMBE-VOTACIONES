a66a956a931087d7ddf77e64a6e6905aef160329
Jose <jfgallegocardona@gmail.com>
2022-10-23 13:50:32 -0500

    SOT-6 Python-Flask 1.3

--- main.py
+++ main.py
@@ -4,3 +4,24 @@ from flask import request
 from flask_cors import CORS
 import json
 from waitress import serve
+
+app=Flask(__name__)
+cors = CORS(app)
+
+def loadFileConfig():
+    with open('config.json') as f:
+        data = json.load(f)
+    return data
+
+###RUTAS
+@app.route("/", methods=['GET'])
+def test():
+    json ={}
+    json["message"] = "server running ..."
+    return jsonify(json)
+
+if __name__=='__main__':
+    dataConfig = loadFileConfig()
+    print("Server running : "+"http://"+dataConfig["url-backend"]+":" + str(dataConfig["port"]))
+    serve(app,host=dataConfig["url-backend"],port=dataConfig["port"])
+
